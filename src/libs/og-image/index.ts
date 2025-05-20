f9e05ca6ae4267a96053cd47ad54753507c23351
hendraaagil <hendraaagil@gmail.com>
2025-05-20 16:43:19 +0700

    feat: modify params

--- src/libs/og-image/index.ts
+++ src/libs/og-image/index.ts
@@ -1,3 +1,4 @@
+import type { CollectionEntry } from 'astro:content'
 import { Resvg } from '@resvg/resvg-js'
 import siteImage from './templates/site'
 import postImage from './templates/post'
@@ -13,7 +14,7 @@ export const generateSiteOgImage = async () => {
 	return svgToPngBuffer(svg)
 }
 
-export const generatePostOgImage = async (title: string, image: string) => {
-	const svg = await postImage(title, image)
+export const generatePostOgImage = async (post: CollectionEntry<'blog'>) => {
+	const svg = await postImage(post.data.title, post.data.heroImage)
 	return svgToPngBuffer(svg)
 }
