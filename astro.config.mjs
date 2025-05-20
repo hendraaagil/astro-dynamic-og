7d28dc52b317e13a34005ec85a010097f4a87aa8
hendraaagil <hendraaagil@gmail.com>
2025-05-20 17:06:15 +0700

    fix: add site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astro-dynamic-og.pages.dev/',
 	vite: {
 		plugins: [tailwindcss()],
 	},
